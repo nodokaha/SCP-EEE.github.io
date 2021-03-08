7f3e5092179543067a7abdaf956f8e566d79426c
5（9-333 <58126606+SCP-EEE@users.noreply.github.com>
2021-03-08 10:16:41 +0900

    Update main.scm

--- main.scm
+++ main.scm
@@ -1,3 +1,3 @@
-(define (fib x) (letrec ((fib-iter (lambda (n f1 f2) (if (< n 1) f1 (fib-iter (- n 1) f2 (+ f1  f2))))))))
+(define (fib x) (letrec ((fib-iter (lambda (n f1 f2) (if (< n 1) f1 (fib-iter (- n 1) f2 (+ f1  f2))))))(fib-iter x 0 1)))
 (console-log "ok")
 (display (string-append "hello world" (number->string (fib 10)) "test"))
